edef4e8d34c393e4b8ea921bad09a0b799ecf438
billmukenga01 <billmukenga01@gmail.com>
2023-06-22 19:34:26 +0300

    feat: added constructor to make new object

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ const container = document.querySelector('container');
 
 let myLibrary = [];
 
-function Book() {
+let title = 'The River and the Source';
+let year = '1973';
+let author = 'Akoko';
+
+
+function Book(title, yearOfPublish, author) {
   // the constructor...
+
+  this.title = title;
+  this.yearOfPublish = yearOfPublish;
+  this.author = author;
+
 }
+
+Book.prototype.addBookToLibrary = addBookToLibrary;
 
 function addBookToLibrary() {
   // do stuff here
+  const book = new Book(title, year, author);
+  myLibrary.push(book);
+
 }
 
 function loop (){
