547c00113fa53181150cc275bb617242b8b82df5
billmukenga01 <billmukenga01@gmail.com>
2023-06-23 23:46:38 +0300

    feat: display the books once added

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ button.addEventListener('click', function(){
 
   title = prompt('Enter Title of Book');
   year = prompt('Enter the Publication Year');
-  title = prompt('Enter the Author of the book');
+  author = prompt('Enter the Author of the book');
 
   addBookToLibrary()
 })
@@ -37,7 +37,7 @@ function addBookToLibrary() {
 
   length = myLibrary.length;
 
-  index = myLibrary.indexOf(Book);
+  index = myLibrary.indexOf(book);
   const style = document.createElement('style');
 
   style.textContent = `
@@ -54,11 +54,55 @@ function addBookToLibrary() {
 function loop (index){
 
   const newDiv = document.createElement('div');
+  const title = document.createElement('div');
+  const author= document.createElement('div');
+  const yearOfPublish = document.createElement('div');
+  const divOne = document.createElement('div');
+  const divTwo = document.createElement('div');
+  const divThree = document.createElement('div');
+  const divFour = document.createElement('div');
+  const divFive = document.createElement('div');
+  const divSix = document.createElement('div');
+
+
   newDiv.setAttribute('class', 'card-item');
+  title.setAttribute('class', 'title');
+  author.setAttribute('class', 'author');
+  yearOfPublish.setAttribute('class', 'yearOfPublish');
+  divOne.setAttribute('class', 'display');
+  divTwo.setAttribute('class', 'content');
+  divThree.setAttribute('class', 'display');
+  divFour.setAttribute('class', 'content');
+  divFive.setAttribute('class', 'display');
+  divSix.setAttribute('class', 'content');
+
+
+
   container.appendChild(newDiv);
-      
+  newDiv.appendChild(title);
+  newDiv.appendChild(author);
+  newDiv.appendChild(yearOfPublish);
+
+  title.appendChild(divOne);
+  title.appendChild(divTwo);
+  author.appendChild(divThree);
+  author.appendChild(divFour);
+  yearOfPublish.appendChild(divFive);
+  yearOfPublish.appendChild(divSix);
+
+
+  divOne.textContent = "TITLE";
+  divThree.textContent = "AUTHOR";
+  divFive.textContent = "YEAR OF PUBLISH";
+
+  divTwo.textContent = myLibrary[index].title;
+  divFour.textContent = myLibrary[index].author;
+  divSix.textContent = myLibrary[index].yearOfPublish;
+
+
+
 }
 
 
 
-// console.log(myLibrary)
+console.log(index)
